d8c93717de8524456b236807c6b44f6450efc51c
jemHerrera <jemPC@.github.com>
2018-05-04 14:27:29 -0600

    fixed ai bug, added messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-
+  $('body').hide().fadeIn(2000);
   //GLOBAL VARIABLES
   let character = '';
   let victoryArr = [
@@ -18,18 +18,6 @@ $('document').ready(function(){
   let gameCount = 0;
   let endGame = false;
 
-  // CHANGE COLOR EVENT
-  $('.colors').on('click', function(event){
-    let color = $(event.target).css('background-color');
-    $(':root').css('--theme-color', color);
-    $(':root').css('--text-color', '#ffffff');
-    $('section').children().css('border', 'none');
-    if (color == 'rgb(255, 255, 255)'){
-      $(':root').css('--text-color', 'rgb(0,0,0)');
-      $('section').children().css('color', 'rgb(0,0,0)');
-      $('section').children().css('border', '1px solid #000');
-    }
-  });
   // CHOOSING WHETHER X OR O
   $('.char').on('mouseover', function(char){
     $('.char').css('opacity', '0.5');
@@ -41,32 +29,35 @@ $('document').ready(function(){
   $('.char').on('click', function(char){
     character = $(char.target).text();
     $('.xo').css('display', 'none');
-    $('section').css('display', 'grid');
+    $('section').css('display', 'grid').hide().fadeIn(2000);
     $('#your').hide().fadeIn(300).css('display', 'block');
   });
 
   //MAIN LOGIC SEQUENCE
-    $('section').on('click', function(event){
+  $('section').on('click', function(event){
 
-      //check if the spot is not yet occupied
-      let id = event.target.id;
-      if(targets.indexOf(id) != -1 && playerArr.length <= robotArr.length && your.style.display == 'block'){
+    //check if the spot is not yet occupied
+    let id = event.target.id;
+    if(targets.indexOf(id) != -1 && playerArr.length <= robotArr.length && your.style.display == 'block'){
 
-        //display "opponents turn!"
-        $('#your').css('display', 'none');
-        $('#opponent').hide().fadeIn(300).css('display', 'block');
+      //display "opponents turn!"
+      $('#your').css('display', 'none');
+      $('#opponent').hide().fadeIn(300).css('display', 'block');
 
-        //display X or O
-        let block = $(event.target);
-        block.text(character);
-        playerArr.push(targets.splice(targets.indexOf(id), 1)[0]);
-        victoryCheck(playerArr, victoryArr, targets);
+      //display X or O
+      let block = $(event.target);
+      block.text(character);
+      playerArr.push(targets.splice(targets.indexOf(id), 1)[0]);
+      victoryCheck(playerArr, victoryArr, targets);
 
-        //robot moves
-        if(endGame == false) setTimeout(function(){robot();}, 1000);
+      //robot moves
+      if(endGame == false) setTimeout(function(){robot();}, 1000);
 
-      }
-    });
+    }
+  });
+
+  // CHANGE COLOR EVENT
+  $('.colors').on('click', (event) => $(':root').css('--theme-color', $(event.target).css('background-color')));
 
   //FUNCTIONS
   //MAIN AI
@@ -76,7 +67,7 @@ $('document').ready(function(){
 
     //smart robot choices
     let robotChoice = '';
-    //if beginning of the match, take middle or edges randomly
+    //if beginning of the match, //robot keeps on choosing targets until it finds an edge
     if(robotArr.length == 0){
       while(['2','4','6','8',''].indexOf(robotChoice)!= -1){
         robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
@@ -85,15 +76,24 @@ $('document').ready(function(){
       if(playerArr.length == 1 && playerArr[0] !=5) robotChoice = '5';
     }
 
-    //if in the middle of the match
+    //if not the beginning of the match
     else{
       //check if robot has 2 consecutive, and finish if necessary
       robotChoice = complete(robotArr, victoryArr, targets);
-      //check if player has 2 consecutive, and deny the player victory
+      //check if player has 2 consecutive, and deny the player from victory
       if(!robotChoice) {
         robotChoice = complete(playerArr, victoryArr, targets);
       }
-      //if there are no 2 consecutives, pick a spot at random
+      //prioritize edges if there are no 2 consecutives
+      if(!robotChoice ){
+        let limitLoop = 0;
+        while(['2','4','6','8',''].indexOf(robotChoice) > -1 || !robotChoice){
+          robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
+          if(limitLoop > 1000) break;
+          limitLoop++;
+        }
+      }
+      //if there are neither 2 consecutives nor edges left, pick a spot at random
       if(!robotChoice){
           robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
       }
@@ -139,12 +139,42 @@ $('document').ready(function(){
       if (count == 3){
         $('h2').css('display', 'none');
         $('#defeat').hide().fadeIn(300).css('display', 'block');
+        
         reset();
       }
     }
   }
   //RESET FUNCTION
   function reset(){
+
+    //Generate messages
+    if (gameCount == 0) $('.spoiler').hide().fadeIn(2000).text("Spoiler alert: Nobody won this game.. so far");
+    else if (gameCount == 1) $('.spoiler').hide().fadeIn(2000).text("But if you actually win... Send me an email, my robot and I need to talk");
+    else if (gameCount == 3) $('.spoiler').hide().fadeIn(2000).text("I'm quite impressed with your persistence");
+    else if (gameCount == 4) $('.spoiler').hide().fadeIn(2000).text("By the way, my name is Jem, nice to meet you");
+    else if (gameCount == 5) $('.spoiler').hide().fadeIn(2000).text("Hopefully, you're aware that this is pre-written");
+    else if (gameCount == 6) $('.spoiler').hide().fadeIn(2000).text("This is your 7th game with my robot, just fyi");
+    else if (gameCount == 7) $('.spoiler').hide().fadeIn(2000).text("Congratulations!, you just wasted minutes of your life.. Reading pre-written text.. and getting draws with an AI");
+    else if (gameCount == 8) $('.spoiler').hide().fadeIn(2000).text("Any normal person would never reach this content, are you sure you don't need medical attention?");
+    else if (gameCount == 9) {
+      $('.spoiler').hide().fadeIn(2000).text("Don't you think this is getting a little boring? Ok I give up, you win, here's your rainbow.");
+      setTimeout(function(){
+        $('.colors').css('display','grid').hide().fadeIn(2000);
+        $('.spoiler').hide();
+      }, 7000);
+    }
+    else if (gameCount == 10) {
+      $('.colors').hide();
+      $('.spoiler').hide().fadeIn(2000).text("Ha! No more rainbows for you!");
+    }
+    else if (gameCount == 11) {
+      $('.spoiler').hide().fadeIn(2000).text("Just kidding! Well, it's been a pleasure, thank you for spending your time here. Goodbye.");
+      setTimeout(function(){
+        $('.colors').css('display','grid').hide().fadeIn(2000);
+        $('.spoiler').hide();
+      }, 7000);
+    }
+    
     endGame = true;
     gameCount++;
     setTimeout(function(){
