c52b15fda18036f066655358fcabecd9f71391b0
jemHerrera <jhneramuz@gmail.com>
2018-08-24 19:22:31 -0600

    changes in numbering

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,15 @@ $('document').ready(function(){
   let character = '',
   victoryArr = [
     ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9'],
-    ['1','4','7'],
-    ['2','5','8'],
-    ['3','6','9'],
-    ['1','5','9'],
-    ['3','5','7']
+    ['4','5','-4'],
+    ['-3','-2','-1'],
+    ['1','4','-3'],
+    ['2','5','-2'],
+    ['3','6','-1'],
+    ['1','5','-1'],
+    ['3','5','-3']
   ],
-  targets = ['1','2','3','4','5','6','7','8','9'],
+  targets = ['1','2','3','4','5','-4','-3','-2','-1'],
   playerArr = [],
   robotArr = [],
   gameCount = 0,
@@ -67,26 +67,41 @@ $('document').ready(function(){
 
     //smart robot choices
     let robotChoice = '';
-    //if beginning of the match, //robot keeps on choosing targets until it finds an edge
+
+
+    //if beginning of the match, robot will target either corners or center
     if(robotArr.length == 0){
-      while(['2','4','6','8',''].indexOf(robotChoice)!= -1){
+      while(['2','4','-4','-2',''].indexOf(robotChoice)!= -1){
         robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
       }
       if(playerArr.length == 1 && playerArr[0] !=5) robotChoice = '5';
     }
+
+    // second turn
+    // offense
+    if(playerArr.length == 1 && robotArr.length == 1){
+      if(['2','4','-4','-2'].indexOf(playerArr[0]) > -1){
+        if(robotArr[0]== 5){
+          while(['2','4','-4','-2','5'].indexOf(robotChoice)!= -1){
+            robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
+          }
+        }
+      }
+    }
+
     //if not the beginning of the match
     else{
       //check if robot has 2 consecutive, and finish if necessary
       robotChoice = complete(robotArr, victoryArr, targets);
-      //check if player has 2 consecutive, and deny the player from victory
+      //check if player has 2 consecutive, and deny the player of victory
       if(!robotChoice) {
         robotChoice = complete(playerArr, victoryArr, targets);
       }
-      if (!robotChoice && playerArr.indexOf('8') == -1 && robotArr.indexOf('8') == -1) robotChoice = '8';
+
       //prioritize edges if there are no 2 consecutives
       if(!robotChoice ){
         let limitLoop = 0;
-        while(['2','4','6','8',''].indexOf(robotChoice) > -1 || !robotChoice){
+        while(['2','4','-4','-2',''].indexOf(robotChoice) > -1 || !robotChoice){
           robotChoice = targets[Math.floor(Math.random()*(targets.length-1))];
           if(limitLoop > 1000) break;
           limitLoop++;
@@ -145,44 +160,13 @@ $('document').ready(function(){
   }
   //RESET FUNCTION
   function reset(){
-
-    //Generate messages
-    if (gameCount == 0) $('.spoiler').hide().fadeIn(2000).text("Spoiler alert: One does not simply win this game");
-    else if (gameCount == 1) $('.spoiler').hide().fadeIn(2000).text("But if you actually win... Send me an email (My robot and I will then have a chat)");
-    else if (gameCount == 3) $('.spoiler').hide().fadeIn(2000).text("I'm quite impressed with your persistence");
-    else if (gameCount == 4) $('.spoiler').hide().fadeIn(2000).text("By the way, my name is Jem, nice to meet you");
-    else if (gameCount == 5) $('.spoiler').hide().fadeIn(2000).text("Umm...Are you a hiring manager? Hire me! I'll promise to behave!");
-    else if (gameCount == 6) $('.spoiler').hide().fadeIn(2000).text("This is your 7th game with my robot, just fyi");
-    else if (gameCount == 7) $('.spoiler').hide().fadeIn(2000).text("Congratulations!, you just wasted minutes of your life.. Reading pre-written text and trying to beat my buddy robot here");
-    else if (gameCount == 8) $('.spoiler').hide().fadeIn(2000).text("No mentally healthy individual will ever get to this point.. Are you sure you don't need any medical attention?");
-    else if (gameCount == 9) $('.spoiler').hide().fadeIn(2000).text("How about you play my simon game? That might be worth your time");
-    else if (gameCount == 11) $('.spoiler').hide().fadeIn(2000).text("Don't you think this is getting a little boring?");
-    else if (gameCount == 12) {
-      $('.spoiler').hide().fadeIn(2000).text("Ok I give up, you win, here's your rainbow.");
-      setTimeout(function(){
-        $('.colors').css('display','grid').hide().fadeIn(2000);
-        $('.spoiler').hide();
-      }, 5000);
-    }
-    else if (gameCount == 13){
-      $('.colors').hide();
-      $('.spoiler').hide().fadeIn(2000).text("Ha! No more rainbows for you!");
-    }
-    else if (gameCount == 15) {
-      $('.spoiler').hide().fadeIn(2000).text("Just kidding! Well, it's been a pleasure, thank you for spending your time here.");
-      setTimeout(function(){
-        $('.colors').css('display','grid').hide().fadeIn(2000);
-        $('.spoiler').hide();
-      }, 7000);
-    }
-    
     endGame = true;
     gameCount++;
     setTimeout(function(){
       $('section').children().text('')
       playerArr = [];
       robotArr = [];
-      targets = ['1','2','3','4','5','6','7','8','9'];
+      targets = ['1','2','3','4','5','-4','-3','-2','-1'];
       $('h2').css('display', 'none');
       if(gameCount%2 != 0){
         $('#opponent').hide().fadeIn(300).css('display', 'block');
